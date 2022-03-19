ed412f0b1f9f7c0b95aef572ab6a7c2c9b681b6a
GitHub Action <action@github.com>
2022-03-19 13:29:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/cherrydevbomb/collabo/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/cherrydevbomb/collabo/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.cherrydevbomb.collabo.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.cherrydevbomb.collabo.MyBundle
 
 class MyApplicationService {
 
