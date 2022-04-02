996be82d002c8b6f94a84e27a8b1940827fd5c77
cherryDevBomb <turcan.olguta@gmail.com>
2022-04-02 16:19:34 +0300

    initialize file in editor

--- src/main/java/com/github/cherrydevbomb/collabo/communication/subscriber/PeerInitStateTransferSubscriber.java
+++ src/main/java/com/github/cherrydevbomb/collabo/communication/subscriber/PeerInitStateTransferSubscriber.java
@@ -6,9 +6,6 @@ import com.github.cherrydevbomb.collabo.communication.model.InitialState;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ReadAction;
 import com.intellij.openapi.application.WriteAction;
-import com.intellij.openapi.editor.Document;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.editor.EditorFactory;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.fileEditor.FileEditorManager;
 import com.intellij.openapi.fileTypes.FileType;
@@ -16,14 +13,15 @@ import com.intellij.openapi.fileTypes.FileTypeRegistry;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiDirectory;
-import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiFileFactory;
 import com.intellij.psi.impl.file.PsiDirectoryFactory;
 import io.lettuce.core.pubsub.RedisPubSubListener;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Stream;
 
 @Slf4j
 public class PeerInitStateTransferSubscriber implements RedisPubSubListener<String, String> {
@@ -53,11 +51,25 @@ public class PeerInitStateTransferSubscriber implements RedisPubSubListener<Stri
         ApplicationManager.getApplication().invokeAndWait(() -> psiFile.set(WriteAction.compute(() -> PsiFileFactory.getInstance(project).createFileFromText(initialState.getFileName(), fileType, initialState.getText()))));
 
         ApplicationManager.getApplication().invokeLater(() -> {
-            PsiDirectory psiDirectory = WriteAction.compute(() -> PsiDirectoryFactory.getInstance(project).createDirectory(project.getProjectFile().getParent()).createSubdirectory("collabo"));
-            PsiElement psiFileAdded = WriteAction.compute(() -> psiDirectory.add(psiFile.get()));
-            VirtualFile virtualFile = WriteAction.compute(() -> psiDirectory.findFile(initialState.getFileName()).getVirtualFile());
-            Document document = WriteAction.compute(() -> FileDocumentManager.getInstance().getDocument(virtualFile));
-            Editor editor = WriteAction.compute(() -> EditorFactory.getInstance().createEditor(document, project, fileType, false)); // TODO check if needed
+            PsiDirectory projectRoot = ReadAction.compute(() -> PsiDirectoryFactory.getInstance(project).createDirectory(project.getProjectFile().getParent()));
+            PsiDirectory existingCollaboDirectory = WriteAction.compute(() -> projectRoot.findSubdirectory("collabo"));
+            PsiDirectory collaboDirectory = (existingCollaboDirectory != null) ? existingCollaboDirectory : WriteAction.compute(() -> projectRoot.createSubdirectory("collabo"));
+
+            // delete existing files left over from previous sessions
+            WriteAction.compute(() -> {
+                Stream.of(collaboDirectory.getFiles()).forEach(file -> {
+                    try {
+                        file.getVirtualFile().delete(null);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+                return null;
+            });
+
+            // create and show the transferred file in the editor
+            WriteAction.compute(() -> collaboDirectory.add(psiFile.get()));
+            VirtualFile virtualFile = ReadAction.compute(() -> collaboDirectory.findFile(initialState.getFileName()).getVirtualFile());
             ReadAction.compute(() -> FileEditorManager.getInstance(project).openFile(virtualFile, true));
             FileDocumentManager.getInstance().saveAllDocuments();
         });
